2069fe82330bbc5ecc1db0c43b55a050a8ef08b9
Keanu Robles <Keanu@Keanus-MacBook-Pro.local>
2016-10-11 22:46:12 -1000

    Part of Databases Part 2

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -5,7 +5,7 @@ if(Meteor.isClient){
 
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, { sort: {score: -1, name: 1} });
     },
     'selectedClass': function(){
       var playerId = this._id;
@@ -21,8 +21,16 @@ if(Meteor.isClient){
       var playerId = this._id;
       Session.set('selectedPlayer', playerId);
       var selectedPlayer = Session.get('selectedPlayer');
-      console.log(selectedPlayer);
-    }
+
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5 }});
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+    },
   });
 }
 
